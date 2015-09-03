de6dd85d262ec2a9420ca2d0b1754f2fba2b239c
H. Omer Sensoy <omer.sensoy@vngrs.com>
2015-09-03 09:29:55 +0300

    Initial commit for old d3 files

---
+++ calendar_d3/01_calendar.js
@@ -0,0 +1,50 @@
+var d3 = require('d3');
+var Calendar = require('calendar').Calendar;
+var cal = new Calendar();
+var consts = require('./consts');
+
+var month = 11;
+var year = 2014;
+/*cal.monthDays(2014, 11).forEach(function(week) {
+  console.log(week);
+});*/
+
+var weeks = cal.monthDays(year, month);
+var table = d3.select('#calendar');
+
+var header = table.append('thead');
+var body = table.append('tbody');
+
+header
+  .append('tr')
+  .append('td')
+  .attr('colspan', 7)
+  .style('text-align', 'center')
+  .text(consts.monthNames[month]);
+
+header
+  .append('tr')
+  .selectAll('td')
+  .data(consts.dayNames)
+  .enter()
+  .append('td')
+  .style('text-align', 'center')
+  .text(function(d){
+    return d;
+  });
+
+weeks.forEach(function(week) {
+  body
+    .append('tr')
+    .selectAll('td')
+    .data(week)
+    .enter()
+    .append('td')
+    .attr('class', function(d) {
+      return d > 0 ? '' : 'empty';
+    })
+    .text(function(d) {
+      // Only return day if its greater than 0... if inside current month
+      return d > 0 ? d : '';
+    })
+});
---
+++ calendar_d3/consts.js
@@ -0,0 +1,6 @@
+var appConstants = {
+  monthNames: ['Ocak', 'Subat', 'Mart', 'Nisan', 'Mayis', 'Haziran', 'Temmuz', 'Agustos', 'Eylul', 'Ekim', 'Kasim', 'Aralik'],
+  dayNames: ['Pazartesi','Sali','Carsamba','Persembe','Cuma','Cumartesi','Pazar',]
+}
+
+module.exports = appConstants;
